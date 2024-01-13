fe6ccdf8e857d8d07bad124a325130c0ada775df
Deniz <wi21232@lehre.dhbw-stuttgart.de>
2024-01-13 19:53:46 +0100

    third commit, should work now

--- src/components/MapComponent/index.js
+++ src/components/MapComponent/index.js
@@ -1,58 +1,37 @@
 import React, { useEffect, useRef, useState } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import markerIconUrl from 'leaflet/dist/images/marker-icon.png';
-import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const MapComponent = () => {
   const [markersData, setMarkersData] = useState([]);
   const mapRef = useRef(null);
 
-  // Function to fetch markers data from the backend
-  const fetchMarkersData = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/api/markers');
-      const data = await response.json();
-      setMarkersData(data);
-    } catch (error) {
-      console.error('Error fetching markers data:', error);
-    }
-  };
-
   useEffect(() => {
-    // Fetch markers data when the component mounts
-    fetchMarkersData();
+    // Dynamically import Leaflet and its CSS only on the client side
+    const importLeaflet = async () => {
+      const L = await import('leaflet');
+      await import('leaflet/dist/leaflet.css');
 
-    if (typeof window !== 'undefined') {
-      // Initialize the map
-      const map = L.map('map').setView([48.77318, 9.17082], 11); 
+      // Define a custom icon using the local paths
+      const customIcon = L.icon({
+        iconUrl: '/img/marker-icon.png',
+        shadowUrl: '/img/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      // Initialize the map here after Leaflet is imported
+      mapRef.current = L.map('map').setView([48.77318, 9.17082], 11);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-      mapRef.current = map;
-    }
+      }).addTo(mapRef.current);
 
-    return () => {
-      if (mapRef.current) {
-        mapRef.current.remove();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // Define a custom icon using the imported URLs
-    const customIcon = L.icon({
-      iconUrl: markerIconUrl,
-      shadowUrl: markerShadowUrl,
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
-
-    // Add markers to the map when markersData is updated
-    if (mapRef.current && markersData.length > 0) {
-      markersData.forEach(marker => {
+      // Fetch markers data and add markers to the map
+      const data = await fetchMarkersData();
+      setMarkersData(data);
+      // Add markers to the map using the data and custom icon
+      data.forEach(marker => {
         const { lat, long, homename, sensors } = marker;
         const markerInfo = L.marker([lat, long], { icon: customIcon }).addTo(mapRef.current);
         let popupContent = `<strong>${homename}</strong><br>`;
@@ -63,10 +42,36 @@ const MapComponent = () => {
         });
         markerInfo.bindPopup(popupContent);
       });
-    }
-  }, [markersData]);
+    };
 
-  return <div id="map" style={{ height: '750px', width: '100%' }}></div>;
+    importLeaflet();
+
+    return () => {
+      // Clean up the map when the component is unmounted
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
+    };
+  }, []);
+
+  // Function to fetch markers data from the backend
+  const fetchMarkersData = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/markers');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching markers data:', error);
+    }
+  };
+
+  return (
+    <BrowserOnly>
+      {() => (
+        <div id="map" style={{ height: '750px', width: '100%' }}></div>
+      )}
+    </BrowserOnly>
+  );
 };
 
 export default MapComponent;
